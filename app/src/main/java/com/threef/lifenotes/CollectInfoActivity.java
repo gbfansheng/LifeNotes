bbc03cc460a0c12dc3f4c36984c47624c8f204b7
Shenglin Fan <shenglinfan@ShenglindeMacBook-Pro.local>
2016-01-30 20:10:28 +0800

    完成信息收集UI面

---
+++ app/src/main/java/com/threef/lifenotes/CollectInfoActivity.java
@@ -0,0 +1,28 @@
+package com.threef.lifenotes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CollectInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect_info);
+
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //调用网络
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
